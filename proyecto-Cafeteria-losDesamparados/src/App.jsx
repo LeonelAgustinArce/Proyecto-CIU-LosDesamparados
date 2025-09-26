034f7febef434e2ab9e7e7861d3261fe4b4fd39a
cadoredr <cador.edr@gmail.com>
2025-09-26 02:58:11 -0300

    se agrego el footer y se reubico la navbar

--- proyecto-Cafeteria-losDesamparados/src/App.jsx
+++ proyecto-Cafeteria-losDesamparados/src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Carta from './pages/Carta';
 import Carrito from './pages/Carrito';
 import Reserva from './pages/Reserva';
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -18,6 +20,7 @@ function App() {
           <Route path="/reserva" element={<Reserva />} />
         </Routes>
       </main>
+      < Footer />
     </div>
   );
 }
