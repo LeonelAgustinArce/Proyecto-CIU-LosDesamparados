cfb676d86713eb2d8e069ce6a219252c7f04cca5
LeonelAgustinArce <leonelagustin.arce@estudiantes.unahur.edu.ar>
2025-09-28 15:36:30 -0300

    Añadi fotos y cafes nuevos, renombre los cafes, cambie el logo de la pestaña, modifique alfabeticamente la carta, modifique los precios, cree un archivo cartafiltro que despues voy a profundizar

--- proyecto-Cafeteria-losDesamparados/src/pages/Carta.jsx
+++ proyecto-Cafeteria-losDesamparados/src/pages/Carta.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import cafe01 from '../assets/cafe-01.png';
-import cafe02 from '../assets/cafe-02.png';
-import cafe03 from '../assets/cafe-03.png';
-import cafe04 from '../assets/cafe-04.png';
-import cafe05 from '../assets/cafe-05.png';
-import cafe06 from '../assets/cafe-06.png';
-import cafe07 from '../assets/cafe-07.png';
-import cafe08 from '../assets/cafe-08.png';
+import Americano from '../assets/cafe-01.png';
+import Latte from '../assets/cafe-02.png';
+import Cappuccino from '../assets/cafe-03.png';
+import Mocha from '../assets/cafe-04.png';
+import Espresso from '../assets/cafe-05.png';
+import Macchiato from '../assets/cafe-06.png';
+import IcedCoffe from '../assets/cafe-07.png';
+import LatteMacchiato from '../assets/cafe-latte-macchiato.png';
+import CaramelMacchiato from '../assets/cafe-caramel-macchiato.png'
 
 function Carta() {
   const productos = [
-    { id: 8, nombre: 'Café con Leche', imagen: cafe08, precio: 1 },
-    { id: 4, nombre: 'Café Mocha',     imagen: cafe04, precio: 1 },
-    { id: 1, nombre: 'Americano',      imagen: cafe01, precio: 1 },
-    { id: 5, nombre: 'Espresso',       imagen: cafe05, precio: 1 },
-    { id: 2, nombre: 'Latte',          imagen: cafe02, precio: 1 },
-    { id: 6, nombre: 'Macchiato',      imagen: cafe06, precio: 1 },
-    { id: 3, nombre: 'Cappuccino',     imagen: cafe03, precio: 1 },
-    { id: 7, nombre: 'Iced Coffee',    imagen: cafe07, precio: 1 },
+    { id: 1, nombre: 'Americano'        , imagen: Americano, precio: 6200 },
+    { id: 2, nombre: 'Café Mocha'       , imagen: Mocha, precio: 7200 },
+    { id: 3, nombre: 'Cappuccino'       , imagen: Cappuccino, precio: 7800 },
+    { id: 4, nombre: 'Caramel Macchiato', imagen: CaramelMacchiato, precio: 10000 },
+    { id: 5, nombre: 'Espresso'         , imagen: Espresso, precio: 8500 },
+    { id: 6, nombre: 'Iced Coffee'      , imagen: IcedCoffe, precio: 9800 },
+    { id: 7, nombre: 'Latte Macchiato'  , imagen: LatteMacchiato, precio: 9800 },
+    { id: 8, nombre: 'Latte'            , imagen: Latte, precio: 7200 },
+    { id: 9, nombre: 'Macchiato'        , imagen: Macchiato, precio: 7200 },
+   
     
   ];
 
@@ -26,7 +29,7 @@ function Carta() {
       <div className="row">
         <div className="col-12">
           <h2>
-            Carta
+            Carta de Café
           </h2>
           <div>
               <p>
@@ -43,8 +46,9 @@ function Carta() {
               <img src={producto.imagen} className="card-img-top" alt={producto.nombre} />
               <div className="card-body">
                 <h5 className="card-title">{producto.nombre}</h5>
-                <p class="card-text">Precio: ${producto.precio}</p>
-                <button className="btn-minimal">+</button>
+                <p className="card-text"  >Precio: ${producto.precio} 
+                <button className="btn-minimal"> + </button> 
+                </p>
               </div>
             </div>
           </div>
